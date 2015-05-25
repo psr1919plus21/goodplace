db29bae45984d189b870a369a420d4e40d93e13c
Filipp Gaponenko <psr1919plus21@gmail.com>
2015-05-26 01:05:19 +0300

    :wrench: homepage done

---
+++ _footer.php
@@ -0,0 +1,10 @@
+ 
+	<footer class="l-footer">
+		<div class="l-restrictor">
+			<div><a href="email:psr1919plus21@gmail.com" class="footer-author">psr1919plus21@gmail.com</a></div>
+			<div class="footer-year">2015</div>	
+		</div>
+	</footer>
+
+ </body>
+</html>
